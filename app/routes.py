772d2d10b614bea47089334a35e9ddabebc3c3e9
BIMseven <bim7@pitt.edu>
2019-05-03 07:45:21 -0400

    user auth and login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
 from app import app
 from app.forms import LoginForm
+from app.models import User
 #-----------------------------------------------------------------------------------FIELDS
 #-----------------------------------------------------------------------------------ROUTES
 @app.route('/')
@@ -23,9 +25,15 @@ def index():
     handles user creation """
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:       
+        return redirect(url_for('index')) # Return the logged in user to the homepage?
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('/index'))
+        # Get the user from the database
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            # user does not exist or the wrong password has been entered
+            flash('Invalid username or password')
+        login_user(user, remember=form.remember_me.data) # Log in the User
+        return redirect(url_for('/index')) #redirect to the home page with the user logged in
     return render_template('login.html', title='Sign In', form=form)
